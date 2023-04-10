a10d92881d469476ac073f04a7248c01cb9e3e35
Kirill <abqwerty390@gmail.com>
2023-04-10 16:59:49 +0300

    add lesson22

---
+++ 22lesson/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField] private float _speed = 6f;//скорость передвижения игрока
+
+    private Vector3 _moveDirection;//направление движения игрока
+    private Animator _animator;
+
+}
